612978938991258f6bbb00572d90f889323e386b
Guilherme_Tamega <guitamega06@gmail.com>
2021-12-07 19:23:20 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,18 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de átomo e React Avançado escrito ao lado"
+    />
+    <S.Title>Guilherme Tâmega</S.Title>
+    <S.Description>Desenvolvedor de programas esquisitos</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Homem sentado em uma cadeira em frente a uma grande tela com códigos"
+    />
+  </S.Wrapper>
+)
+
+export default Main
