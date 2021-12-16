541fcd7abb6c4231a1ecc224bb82e1601d2514b5
Guilherme_Tamega <guitamega06@gmail.com>
2021-12-15 21:07:14 -0300

    Progress bar and map style working

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -15,6 +15,20 @@ export type MapProps = {
   places: Place[]
 }
 
+const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY
+const MAPBOX_USERID = process.env.NEXT_PUBLIC_MAPBOX_USERID
+const MAPBOX_STYLEID = process.env.NEXT_PUBLIC_MAPBOX_STYLEID
+
+const CustomTileLayer = () => {
+  return MAPBOX_API_KEY ? (
+    <TileLayer
+      url={`https://api.mapbox.com/styles/v1/${MAPBOX_USERID}/${MAPBOX_STYLEID}/tiles/256/{z}/{x}/{y}@2x?access_token=${MAPBOX_API_KEY}`}
+    />
+  ) : (
+    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+  )
+}
+
 const Map = ({ places }: MapProps) => {
   const router = useRouter()
 
@@ -25,7 +39,7 @@ const Map = ({ places }: MapProps) => {
       scrollWheelZoom={true}
       style={{ height: '100%', width: '100%' }}
     >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <CustomTileLayer />
 
       {places?.map(({ id, slug, name, location }) => {
         const { latitude, longitude } = location
