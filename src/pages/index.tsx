2fecba6a05587ff5ca0c0b00180acf3dad8901d1
Guilherme_Tamega <guitamega06@gmail.com>
2021-12-15 21:54:17 -0300

    Updating performance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ export default function Home({ places }: MapProps) {
 export const getStaticProps = async () => {
   const { places } = await client.request<GetPlacesQuery>(GET_PLACES)
   return {
+    revalidate: 5,
     props: {
       places
     }
